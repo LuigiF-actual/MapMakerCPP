06dae4f216206f22ff5bf2a0536ff5b1126bce8c
LuigiF <246063605+LuigiF-actual@users.noreply.github.com>
2026-05-12 11:13:56 +0200

    Add project files.

---
+++ MapMakerC++.cpp
@@ -0,0 +1,12 @@
+﻿// MapMakerC++.cpp : Defines the entry point for the application.
+//
+
+#include "MapMakerC++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
